9a49bade9ca93eb2aba57f9a0cb7dcb5ca1fa604
coder7eeN <phamnguyenanhhuy@gmail.com>
2021-01-13 15:45:23 +0700

    chore: fastlane and actions

--- app/src/androidTest/java/com/huypham/fastlaneandbeyond/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/huypham/fastlaneandbeyond/ExampleInstrumentedTest.kt
@@ -1,24 +1,48 @@
 package com.huypham.fastlaneandbeyond
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.junit.runners.JUnit4
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+import tools.fastlane.screengrab.locale.LocaleTestRule
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * @author huypham on 1/11/21
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(JUnit4::class)
 class ExampleInstrumentedTest {
+    // JVMField needed!
+    @Rule
+    @JvmField
+    val localeTestRule = LocaleTestRule()
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java, false, false)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.huypham.fastlaneandbeyond", appContext.packageName)
+    fun testTakeScreenshot() {
+        activityRule.launchActivity(null)
+
+        // 1. Prepares to take a screenshot of the app
+        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+        Espresso.onView(ViewMatchers.withId(R.id.btnAsk))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // 2. Takes a screenshot of the first screen
+        Screengrab.screenshot("magic_ball_before_click")
+
+        // 3. Selects the Ask button and triggers a click on it
+        Espresso.onView(ViewMatchers.withId(R.id.btnAsk))
+            .perform(ViewActions.click())
+
+        // 4. Take another screenshot
+        Screengrab.screenshot("magic_ball_after_click")
     }
 }
--- app/src/main/java/com/huypham/fastlaneandbeyond/MainActivity.kt
+++ app/src/main/java/com/huypham/fastlaneandbeyond/MainActivity.kt
@@ -1,10 +1,30 @@
 package com.huypham.fastlaneandbeyond
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.huypham.fastlaneandbeyond.databinding.ActivityMainBinding
 
 /**
  * @author huypham on 1/6/21
  */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAsk.setOnClickListener {
+            binding.imgBall.setImageResource(getDices()[(0..4).random()])
+        }
+    }
+
+    private fun getDices(): Array<Int> = arrayOf(
+        R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5
+    )
 }
